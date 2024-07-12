8091ce496abbfe800802db68e950eb88f9f187c4
Brian Ratemo Magucha <brianratemom@gmail.com>
2024-07-12 13:07:23 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord= word.split('').reverse().join('')
+  return  word === reversedWord
 }
 
 /* 
   Add your pseudocode here
+  Define variable that will return true when reversed
+
 */
 
 /*
   Add written explanation of your solution here
+  Define a variable called reversedWord that takes the word in the function then splits the string reverses it and then joins the string back together.
+  If it returns true when reversed then the word is a Palindrome.
 */
 
 // You can run `node index.js` to view these console logs
